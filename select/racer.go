65f070a9275e04112b8b7b62d0cdb0484452c079
Sanjay Wilson <sanjaywilsonktm@gmail.com>
2025-08-15 22:19:46 +0930

    Learn select keyword - wait on multiple channels

---
+++ select/racer.go
@@ -0,0 +1,23 @@
+package learnselect
+
+import (
+	"net/http"
+)
+
+func Racer(url1, url2 string) string {
+	select {
+	case <- ping(url1):
+		return url1
+	case <- ping(url2):
+		return url2
+	}
+}
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func(){
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
+}
