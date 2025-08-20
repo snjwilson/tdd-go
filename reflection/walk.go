f8ba5565cdb1b2f12b1901641fd1d4ae26d4b81b
Sanjay Wilson <sanjaywilsonktm@gmail.com>
2025-08-20 11:18:56 +0930

    Learn advanced reflection techniques - use recursion to traverse arbitrary data structures - only use reflection when absolutely necessary

--- reflection/walk.go
+++ reflection/walk.go
@@ -2,15 +2,46 @@ package reflection
 
 import "reflect"
 
-func walk(x interface{}, fn func(string)) {
-	val := reflect.ValueOf(x)
-	for i := 0; i < val.NumField(); i++ {
-		f := val.Field(i)
-		switch f.Kind() {
-		case reflect.String:
-			fn(f.String())
-		case reflect.Struct:
-			walk(f.Interface(), fn)
+func walk(x interface{}, fn func(input string)) {
+	val := getValue(x)
+	walkValue := func(val reflect.Value) {
+		walk(val.Interface(), fn)
+	}
+	switch val.Kind() {
+	case reflect.String:
+		fn(val.String())
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walkValue(val.Field(i))
+		}
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < val.Len(); i++ {
+			walkValue(val.Index(i))
+		}
+	case reflect.Map:
+		for _, k := range val.MapKeys() {
+			walkValue(val.MapIndex(k))
+		}
+	case reflect.Chan:
+		for {
+			if v, ok := val.Recv(); ok {
+				walkValue(v)
+			} else {
+				break
+			}
+		}
+	case reflect.Func:
+		fnResult := val.Call(nil)
+		for _, v := range fnResult {
+			walkValue(v)
 		}
 	}
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+	if val.Kind() == reflect.Pointer {
+		val = val.Elem()
+	}
+	return val
 }
--- reflection/walk_test.go
+++ reflection/walk_test.go
@@ -44,6 +44,38 @@ func TestWalk(t *testing.T) {
 			}{33, "London"}},
 			[]string{"Chris", "London"},
 		},
+		{
+			"pointers to things",
+			&Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+		{
+			"slices",
+			[]Profile{
+				{33, "London"},
+				{34, "Reykjavík"},
+			},
+			[]string{"London", "Reykjavík"},
+		},
+		{
+			"arrays",
+			[2]Profile{
+				{33, "London"},
+				{34, "Reykjavík"},
+			},
+			[]string{"London", "Reykjavík"},
+		},
+		// {
+		// 	"maps",
+		// 	map[string]string{
+		// 		"Cow":   "Moo",
+		// 		"Sheep": "Baa",
+		// 	},
+		// 	[]string{"Moo", "Baa"},
+		// },
 	}
 
 	for _, test := range cases {
@@ -57,4 +89,71 @@ func TestWalk(t *testing.T) {
 			t.Errorf("expected %v got %v", test.Expected, got)
 		}
 	}
+
+	t.Run("maps", func(t *testing.T) {
+		aMap := map[string]string{
+			"Cow":   "Moo",
+			"Sheep": "Baa",
+		}
+
+		var got []string
+		walk(aMap, func(input string) {
+			got = append(got, input)
+		})
+
+		assertContains := func(t testing.TB, haystack []string, needle string) {
+			t.Helper()
+			contains := false
+			for i := range haystack {
+				if haystack[i] == needle {
+					contains = true
+				}
+			}
+
+			if !contains {
+				t.Errorf("expected map to contain %q but did not find it. Got %v", needle, haystack)
+			}
+		}
+		assertContains(t, got, "Moo")
+		assertContains(t, got, "Baa")
+
+	})
+
+	t.Run("with channels", func(t *testing.T) {
+		aChannel := make(chan Profile)
+
+		go func() {
+			aChannel <- Profile{33, "Berlin"}
+			aChannel <- Profile{34, "Katowice"}
+			close(aChannel)
+		}()
+
+		var got []string
+		want := []string{"Berlin", "Katowice"}
+
+		walk(aChannel, func(input string) {
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+
+	t.Run("with function", func(t *testing.T) {
+		aFunction := func() (Profile, Profile) {
+			return Profile{33, "Berlin"}, Profile{34, "Katowice"}
+		}
+
+		var got []string
+		want := []string{"Berlin", "Katowice"}
+
+		walk(aFunction, func(input string) {
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
 }
